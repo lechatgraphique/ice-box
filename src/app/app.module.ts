e46ff1efda2556aa7a4df3ad03e028a8e04bbd61
Bastien CLEMENT <hello@lechatgraphique.fr>
2021-03-02 22:03:11 +0100

    generate areas component product & routing test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
-import { ProductsComponent } from './areas/components/product/products/products.component';
+import { ListProductsComponent } from './areas/components/product/list-products/list-products.component';
+import { ProductComponent } from './areas/components/product/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductsComponent
+    ListProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
