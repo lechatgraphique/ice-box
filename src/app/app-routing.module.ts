35fa56c43ad735576cd3be27eb25b897e0ff7218
Bastien CLEMENT <hello@lechatgraphique.fr>
2021-03-02 22:03:11 +0100

    generate areas component product & routing test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './areas/components/product/products/products.component';
+import { ListProductsComponent } from './areas/components/product/list-products/list-products.component';
+import { ProductComponent } from './areas/components/product/product/product.component';
 
 const routes: Routes = [
-  { path: 'products', component: ProductsComponent }
+  { path: 'products', component: ListProductsComponent },
+  { path: 'products/1', component: ProductComponent} /* TODO: Route temporaire pour la creation du componant */
 ];
 
 @NgModule({
